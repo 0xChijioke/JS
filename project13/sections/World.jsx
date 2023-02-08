641139d6cf26909a03247a04b75b6dd51a9a0591
EngrGord <chijokeogbuanya@gmail.com>
2023-02-08 21:37:50 +0100

    progress update

--- project13/sections/World.jsx
+++ project13/sections/World.jsx
@@ -7,8 +7,27 @@ import { newFeatures } from "../constants";
 
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} rela
+   z-10`}>
+    <motion.div 
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: "false", amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg;flex-row flex-col gap-8`}>
+        <TypingText title={"| People on the world"} textStyles="text-center" />
+        <TitleText
+          title={<>Track friends</>}
+          textStyles="text-center" />
+          <motion.div
+            variants={fadeIn("up", "tween", 0.3, 1)}
+            className="relative mt-[60px] flex w-full ">
+              <img
+                src="/map.png"
+                alt="map"
+              />
+          </motion.div>
+    </motion.div>
   </section>
 );
 

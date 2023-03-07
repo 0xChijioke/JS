fce2d4d5aead17037f27841f0c5e8265e1f2f129
EngrGord <chijokeogbuanya@gmail.com>
2023-03-07 13:54:56 +0100

    Feedback section finished

--- project13/components/Footer.jsx
+++ project13/components/Footer.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+
 const Footer = () => (
   <footer>
     footer
--- project13/sections/Feedback.jsx
+++ project13/sections/Feedback.jsx
@@ -13,7 +13,7 @@ const Feedback = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-6`}>
         <motion.div
           variants={fadeIn("right", "tween", 0.2, 1)}
-          className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative">
+          className="flex-[0.5] lg:max-w-[370px] flex justify-center flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative">
             <div className="feedback-gradient" />
             <div>
               <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
@@ -23,8 +23,28 @@ const Feedback = () => (
                 Founder | Afroverse
               </p>
             </div>
+            <p className="mt-[24px] font-normal sm:text-[18px] text-[12px] sm:leading-[45px] leading-[39px] text-white">
+              "With the development of today's technology, metaverse is very useful for today's work, or can be called web 3.0. by using metaverse yu can use it as anything.."
+            </p>
         </motion.div>
-
+        <motion.div
+          variants={fadeIn("left", "tween", 0.2, 1)}
+          className="relative flex-1 flex justify-center items-center">
+            <img
+              src="/planet-09.png"
+              alt="planet-09"
+              className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]" 
+            />
+            <motion.div
+              variants={zoomIn(0.4, 1)}
+              className="lg:block hidden absolute -left-[10%] top-[3%]">
+                <img
+                  src="/stamp.png"
+                  alt="stamp"
+                  className="w-[155px] h-[155px] object-contain"
+                />
+            </motion.div>
+        </motion.div>
     </motion.div>
   </section>
 );
